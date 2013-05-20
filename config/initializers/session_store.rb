0d01d8db4273545b7662fcf7116920ea4ab88ca7
DOOLEY NSEWOLO <dooleynsewolo@MacBook-Pro-de-DOOLEY.local>
2013-05-20 13:22:18 +0200

    tempting to deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-DooleyNsewolo::Application.config.session_store :encrypted_cookie_store, key: '_dooley_nsewolo_session'
+#DooleyNsewolo::Application.config.session_store :encrypted_cookie_store, key: '_dooley_nsewolo_session'
+DooleyNsewolo::Application.config.session_store :cookie_store, key: '_dooley_nsewolo_session'
